753498dfb1119e9090899feb9b75b3b884fb9267
Michael Hucka <mhucka@caltech.edu>
2018-03-28 12:16:08 -0700

    Add __version__.py

---
+++ expansionist/__version__.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   CASICS Expansionist version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/expansionist
+# =============================================================================
+
+__version__     = '1.0.0'
+__title__       = 'Expansionist'
+__description__ = 'An expander for tokens in program identifiers'
+__url__         = 'https://github.com/casics/expansionist'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>'
+__email__       = 'casics-team@googlegroups.com'
+__license__     = 'GNU General Public License, version 3.0'
+__copyright__   = 'Copyright (C) 2015-2017 by the California Institute of Technology'
