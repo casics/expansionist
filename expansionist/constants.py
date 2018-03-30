a5f1d2acf98c06ee271f049a838fc916438bcecf
Michael Hucka <mhucka@caltech.edu>
2018-03-29 19:44:37 -0700

    Start a list of acronym definitions

---
+++ expansionist/constants.py
@@ -0,0 +1,47 @@
+'''
+constants: constants used in Expansionist.
+'''
+
+# This is from the NIST Interagency Report 7581 (Sep. 2009) with the following
+# deletions (i.e., none of the following were kept in the list):
+#  - acronyms 1-2 chars long
+#  - acronyms with more than one definition
+#  - acronyms with hyphens in the name
+#  - names of agencies (e.g., ARIN, ARPA, BSI)
+#  - acronyms that are so well known it'd be crazy to expand them (e.g., ASCII)
+
+common_computing_acronyms = {
+    '1xrtt' : 'one times radio transmission technology',
+    '3des'  : 'triple data encryption standard',
+    '3gpp'  : '3rd generation partnership project',
+    '3gpp2' : '3rd generation partnership project 2',
+    'aaa'   : 'authentication, authorization, and accounting',
+    'aaak'  : 'authentication, authorization, and accounting key',
+    'aad'   : 'additional authenticated data',
+    'aar'   : 'after action report',
+    'aas'   : 'adaptive antenna system',
+    'abac'  : 'attribute-based access control',
+    'ace'   : 'access control entry',
+    'acl'   : 'access control list',
+    'acm'   : 'association for computing machinery',
+    'aco'   : 'authenticated cipher offset',
+    'ads'   : 'alternate data stream',
+    'aes'   : 'advanced encryption standard',
+    'afh'   : 'adaptive frequency hopping',
+    'aidc'  : 'automatic identification and data capture',
+    'aim'   : 'association for automatic identification and mobility',
+    'ait'   : 'automatic identification technology',
+    'ajax'  : 'asynchronous javascript and xml',
+    'akid'  : 'authorization key identifier',
+    'akm'   : 'authentication and key management',
+    'alg'   : 'application layer gateway',
+    'ansi'  : 'american national standards institute',
+    'api'   : 'application programming interface',
+    'arp'   : 'address resolution protocol',
+    'aslr'  : 'address space layout randomization',
+    'asn1'  : 'abstract syntax notation 1',
+    'asp'   : 'active server pages',
+    'ata'   : 'advanced technology attachment',
+    'atim'  : 'announcement traffic indication message',
+    'avp'   : 'attribute-value pair',
+}
