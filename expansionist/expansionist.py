14d0215e25f63cb8fe54ddd6611a8b1dc8a58dca
Michael Hucka <mhucka@caltech.edu>
2018-03-29 19:45:01 -0700

    Add ability to expand acronyms and abbreviations

--- expansionist/expansionist.py
+++ expansionist/expansionist.py
@@ -25,15 +25,29 @@ from spiral.constants import suffixes, special_computing_terms
 from nltk.corpus import words as nltk_words
 from nltk.corpus import wordnet as nltk_wordnet
 
+try:
+    from .constants import *
+except:
+    from constants import *
+
 
-# Globals
+# Globals.
 # .............................................................................
 
 dictionary = set(nltk_words.words())
 dictionary.update(nltk_wordnet.all_lemma_names())
 
 
-# Expand
+# Substitutions
+# .............................................................................
+
+def translate(token, substitutions):
+    if not substitutions:
+        return token
+    return substitutions[token] if token in substitutions else token
+
+
+# Expander.
 # .............................................................................
 
 def expressions(token):
@@ -55,27 +69,21 @@ def expansion_candidates(token, context_list):
     # Expand() will use each of the sublists in turn as a source of context.
     # The contexts should go from narrower to wider.
 
-    # Procedure:
-    # 1. if token is a recognized special case, return predefined expansion
-    # 2. if token is a full dictionary word, return it as-is
-    # 3. for context c in context_list:
-    #    a) if token is exact start of a word found in the context, return word
-    #    b) if token ends in 's', remove it and try (a) again
-    #    c) if token wildcard matches a word in the context, return word
-    # The "return word" steps may return multiple candidates, for example if
-    # the context includes multiple words that match..  It's up to the caller
-    # to rank and select from the results.
-
-    if len(token) > 2 and token in dictionary:
+    if len(token) < 2:
+        return [token]
+    # If it's a dictionary word, return that word.
+    if token in dictionary:
         return [token]
     candidates = []
+    # Iterate over the contexts (assumed to be narrower to wider)
     for context in context_list:
-        candidates = [x for x in context if x.startswith(token)]
-        if candidates:
-            return candidates
-        candidates = [x for x in context if re.match(expressions(token), x)]
-        if candidates:
-            return [x for x in candidates if x in dictionary]
+        # If token is an exact start of a word in the context, return word.
+        candidates += [x for x in context if x.startswith(token)]
+        # If token ends in 's', remove it and try (a) again. (Eg. "buffs").
+        if token.endswith('s'):
+            candidates += [x for x in context if x.startswith(token[:-1])]
+        # If token wildcard matches a word in the context, return word
+        candidates += [x for x in context if re.match(expressions(token), x)]
     return candidates if candidates else [token]
 
 
@@ -85,28 +93,49 @@ def expansion_candidates(token, context_list):
 # are equal, take the one that is recognized in our dictionaries,
 # and if there's more than one, take the shortest among them.
 
-def expand(identifier, context_list, as_list = True):
+def expand(identifier, contexts, as_list = True,
+           substitutions = common_computing_acronyms):
     '''Takes the 'identifier', splits it into constituent tokens, and expands
-    the tokens into full words if possible.  Uses 'context_list' as a source
-    of candidate words for the expansion.  'context_list' should be a list of
-    lists, where each sublist represents a context, and the order of the
-    lists is narrower to wider contexts.  Example contexts: function
-    definition, class definition, whole file, directory, whole project.
-    Optional argument 'as_list' indicates whether the result should be
-    returned as a list of words or as a string.  If True, the result will be
-    of the form ['read', 'file', 'input']; if False, it will be a single
+    the tokens into full words if possible.  The process uses 'contexts' as a
+    source of candidate words for the expansion.  The value of 'contexts'
+    should be a list of lists, where each sublist represents a context, and
+    the order of the lists is narrower to wider contexts.  Example contexts:
+    function definition, class definition, whole file, directory, whole
+    project.  Optional argument 'as_list' indicates whether the result should
+    be returned as a list of words or as a string.  If True, the result will
+    be of the form ['read', 'file', 'input']; if False, it will be a single
     string, such as 'read file input'.
+
+    This function will perform substitution of recognized terms such as
+    acronyms and abbreviations; e.g., it will replace 'acl' with 'access
+    control list'.  The optional argument 'substitutions', if supplied, will
+    override Expansionist's default list of substitutions.  The value of
+    'substitutions' should be a dictionary in which each key is a term in
+    lower case (e.g., an acronym) and the value is the desired expansion.
+    Set 'substitutions' to the empty list [] to prevent all expansions; in
+    that case, this function will leave acronyms and abbreviations as-is in
+    the output.
     '''
-    if context_list:
-        if not isinstance(context_list[0], list):
-            raise ValueError('context_list must be a list of lists')
+    if contexts:
+        if not isinstance(contexts[0], list):
+            raise ValueError('contexts must be a list of lists')
     results = []
     for token in ronin.split(identifier):
-        # FIXME doesn't find best, just uses 1st one on the list
-        candidates = expansion_candidates(token, context_list)
+        # FIXME right now it doesn't find best, just uses 1st one on the list
+        candidates = expansion_candidates(token, contexts)
         if candidates:
-            results.append(candidates[0])
+            words = [w for w in candidates if w in dictionary]
+            if words:
+                results.append(words[0])
+            else:
+                results.append(token)
+    if substitutions:
+        final = []
+        for term in results:
+            final += translate(term, substitutions).split()
+    else:
+        final = results
     return results if as_list else ' '.join(results)
 
 
-# print(expand('str', [['string', 'software']]))
+print(expand('asn1_open', [['config', 'configuration']]))
